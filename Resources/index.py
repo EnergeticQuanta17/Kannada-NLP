90f53adf16001a7d8c64361ce7fd3485a6500a21
EnergeticQuanta17 <preetham.trh@gmail.com>
2023-06-12 08:29:22 +0530

    Partial 21 AND Ideas to Implement AND ExtractedPoints

--- Resources/index.py
+++ Resources/index.py
@@ -9,7 +9,14 @@ def index_files(directory, json_file_path):
         file_index = {}
 
     for root, dirs, files in os.walk(directory):
+        if 'Extracted Points' in dirs:
+            dirs.remove('Extracted Points')  # Exclude the directory from further processing
+            
+        print(dirs)
+        print()
+        
         for file in files:
+            print(file)
             file_path = os.path.join(root, file)
             if file_path not in list(file_index.values()):
                 file_index[len(file_index) + 1] = file_path
