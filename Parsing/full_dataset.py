8ff2d8b6e4ef648b3da2103658c8828f281dfb1c
EnergeticQuanta17 <preetham.trh@gmail.com>
2023-06-13 11:33:01 +0530

    6318

--- Parsing/full_dataset.py
+++ Parsing/full_dataset.py
@@ -3,7 +3,9 @@ import pickle
 
 from dataset_parser import extract_list_of_sentences
 
-file_names = os.listdir(r'..\Dataset\DL-DL MT\DL-DL MT')
+# file_names = os.listdir(r'..\Dataset\DL-DL MT\DL-DL MT')
+file_names = os.listdir(r'..\Dataset\dataset_6318\DL-DL MT\DL-DL MT')
+
 
 # for i in file_names:
 #     print(i)
@@ -13,10 +15,11 @@ print(len(file_names))
 all_sentences = []
 sum =0
 for file in file_names:
+    print(file)
     sentences = extract_list_of_sentences(file)
     print(file, len(sentences))
     sum += len(sentences)
     all_sentences += sentences
 print(len(all_sentences), sum)
-with open('full_dataset_113.pickle', 'wb') as file:
+with open('full_dataset_131.pickle', 'wb') as file:
     pickle.dump(all_sentences, file)
