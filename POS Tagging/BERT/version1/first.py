ddf60ba00f9a162817632aad0a3163188fd735f9
EnergeticQuanta17 <preetham.trh@gmail.com>
2023-06-13 10:19:07 +0530

    global start

--- POS Tagging/BERT/version1/first.py
+++ POS Tagging/BERT/version1/first.py
@@ -14,6 +14,7 @@ import sys
 
 import time
 start = time.time()
+global start
 
 sys.path.append('../../../Parsing/')
 print(os.getcwd())
@@ -167,6 +168,7 @@ def train(model, iterator, optimizer, criterion):
         optimizer.step()
 
         if i%10==0: # monitoring
+            global start
             print("step: {}, loss: {}, time: {}".format(i, loss.item(), time.time()-start))
             start = time.time()
     
