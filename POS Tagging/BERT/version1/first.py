9072e7b541346d49dd7cbd3cee088f2e968a76bf
EnergeticQuanta17 <preetham.trh@gmail.com>
2023-06-14 11:40:55 +0530

    logits

--- POS Tagging/BERT/version1/first.py
+++ POS Tagging/BERT/version1/first.py
@@ -128,14 +128,45 @@ def pad(batch):
 
 # Model
 
+# class Net(nn.Module):
+#     def __init__(self, vocab_size=None):
+#         super().__init__()
+#         self.bert = BertModel.from_pretrained('bert-base-cased')
+
+#         # self.fc = nn.Linear(768, vocab_size)
+#         # self.device = device
+        
+#         self.dropout = nn.Dropout(0.2)
+#         self.fc1 = nn.Linear(768, 256)
+#         self.fc2 = nn.Linear(256, vocab_size)
+#         self.device = device
+
+#     def forward(self, x, y):
+#         '''
+#         x: (N, T). int64
+#         y: (N, T). int64
+#         '''
+#         x = x.to(device)
+#         y = y.to(device)
+        
+#         if self.training:
+#             self.bert.train()
+#             encoded_layers, _ = self.bert(x)
+#             enc = encoded_layers[-1]
+#         else:
+#             self.bert.eval()
+#             with torch.no_grad():
+#                 encoded_layers, _ = self.bert(x)
+#                 enc = encoded_layers[-1]
+        
+#         logits = self.fc(enc)
+#         y_hat = logits.argmax(-1)
+#         return logits, y, y_hat
+
 class Net(nn.Module):
     def __init__(self, vocab_size=None):
         super().__init__()
         self.bert = BertModel.from_pretrained('bert-base-cased')
-
-        # self.fc = nn.Linear(768, vocab_size)
-        # self.device = device
-        
         self.dropout = nn.Dropout(0.2)
         self.fc1 = nn.Linear(768, 256)
         self.fc2 = nn.Linear(256, vocab_size)
@@ -146,8 +177,8 @@ class Net(nn.Module):
         x: (N, T). int64
         y: (N, T). int64
         '''
-        x = x.to(device)
-        y = y.to(device)
+        x = x.to(self.device)
+        y = y.to(self.device)
         
         if self.training:
             self.bert.train()
@@ -159,9 +190,12 @@ class Net(nn.Module):
                 encoded_layers, _ = self.bert(x)
                 enc = encoded_layers[-1]
         
-        logits = self.fc(enc)
+        enc = self.dropout(enc)
+        enc = self.fc1(enc)
+        logits = self.fc2(enc)
         y_hat = logits.argmax(-1)
         return logits, y, y_hat
+
 
 def train(model, iterator, optimizer, criterion):
     model.train()
