1c0fb0e1c315b5599a2df2d9111974375417830a
EnergeticQuanta17 <preetham.trh@gmail.com>
2023-06-14 14:53:57 +0530

    returncode

--- pusher.py
+++ pusher.py
@@ -1,5 +1,12 @@
 import subprocess
+import winsound
 
 subprocess.run(['git', 'add', '.'])
 subprocess.run(['git', 'commit', '-m', input('Enter the commit message: ')])
-subprocess.run(['git', 'push',])
+ret = subprocess.run(['git', 'push'])
+print(ret)
+
+if ret.returncode == 0:
+    print("Push successful")
+else:
+    print("Push failed")
