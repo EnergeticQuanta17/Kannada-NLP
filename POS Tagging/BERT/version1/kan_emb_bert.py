5a6112ea5f503432ea335f9f2fb7b58c7c1d40d4
EnergeticQuanta17 <preetham.trh@gmail.com>
2023-06-16 10:48:17 +0530

    ()

--- POS Tagging/BERT/version1/kan_emb_bert.py
+++ POS Tagging/BERT/version1/kan_emb_bert.py
@@ -221,7 +221,7 @@ class Net(nn.Module):
         super().__init__()
         self.bert = BertModel.from_pretrained(BERT_MODEL)
         
-        print(lenself.bert.embeddings.word_embeddings())
+        print(len(self.bert.embeddings.word_embeddings()))
 
         new_word_embeddings = nn.Embedding(custom_embedding.num_embeddings, custom_embedding.embedding_dim)
         new_word_embeddings.weight.data.copy_(custom_embedding.weight.data)
