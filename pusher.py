aa7b426b34dfbb98d72a88864defb2b2c9172c7b
EnergeticQuanta17 <preetham.trh@gmail.com>
2023-06-16 10:57:33 +0530

    pusher

--- pusher.py
+++ pusher.py
@@ -6,6 +6,12 @@ subprocess.run(['git', 'commit', '-m', input('Enter the commit message: ')])
 result = subprocess.run(['git', 'push'])
 print(result)
 
+
+while(result.returncode != 0):
+    subprocess.run(['python', 'conf.py'])
+    result = subprocess.run(['git', 'push'])
+    print(result)
+
 if result.returncode == 0:
     print("Push successful")
     winsound.Beep(500, 500)
