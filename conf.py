d71c4546b1014e67322b880504c1a96a8bafb111
EnergeticQuanta17 <preetham.trh@gmail.com>
2023-06-16 09:44:47 +0530

    custom transformers

--- conf.py
+++ conf.py
@@ -1,3 +1,4 @@
 import subprocess
 subprocess.run(['git', 'config', '--global', '--unset', 'http.proxy'])
-
+subprocess.run(['git', 'config', '--global', '--unset', 'https.proxy'])
+subprocess.run(['git', 'pull'])
