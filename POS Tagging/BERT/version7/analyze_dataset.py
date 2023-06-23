117e4d77277ce6a7d0ce15f5c9afeac91c1e929b
AudaciousVisionary <preetham.trh@gmail.com>
2023-06-23 10:36:55 +0530

    tqmd

--- POS Tagging/BERT/version7/analyze_dataset.py
+++ POS Tagging/BERT/version7/analyze_dataset.py
@@ -1,7 +1,13 @@
+from tqdm import tqdm
+
 count_words = 0
 count_lines = 0
 with open('kn.txt', 'r') as f:
-    for line in f:
+    total_lines = sum(1 for _ in f)
+    f.seek(0)
+
+
+    for i, line in tqdm(enumerate(f), total=total_lines):
         count_lines+=1
 
         count_words += len(line.split(' ')) + 1
