bbefdb259d281256c6445b3c6dcc7e92d931bea5
AudaciousVisionary <preetham.trh@gmail.com>
2023-06-23 10:14:05 +0530

    test

--- POS Tagging/BERT/version7/download_dataset.py
+++ POS Tagging/BERT/version7/download_dataset.py
@@ -3,5 +3,5 @@ import datasets
 IndicCOPA = datasets.load_dataset('ai4bharat/IndicCOPA', 'translation-kn')
 
 print(IndicCOPA.keys())
-print(IndicCOPA['train'][0])
+print(IndicCOPA['test'][0])
 print(type(IndicCOPA))
