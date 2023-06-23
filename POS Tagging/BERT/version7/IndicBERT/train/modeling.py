730e8c33b444a248049ab27a12cb7aef54f03519
AudaciousVisionary <preetham.trh@gmail.com>
2023-06-23 14:39:02 +0530

    get var

--- POS Tagging/BERT/version7/IndicBERT/train/modeling.py
+++ POS Tagging/BERT/version7/IndicBERT/train/modeling.py
@@ -406,7 +406,7 @@ def embedding_lookup(input_ids,
     if input_ids.shape.ndims == 2:
         input_ids = tf.expand_dims(input_ids, axis=[-1])
 
-    embedding_table = tf.get_variable(
+    embedding_table = tf.compat.v1.get_variable(
                     name=word_embedding_name,
                     shape=[vocab_size, embedding_size],
                     initializer=create_initializer(initializer_range))
@@ -473,7 +473,7 @@ def embedding_postprocessor(input_tensor,
         if token_type_ids is None:
             raise ValueError("`token_type_ids` must be specified if"
                                                                              "`use_token_type` is True.")
-        token_type_table = tf.get_variable(
+        token_type_table = tf.compat.v1.get_variable(
                         name=token_type_embedding_name,
                         shape=[token_type_vocab_size, width],
                         initializer=create_initializer(initializer_range))
@@ -489,7 +489,7 @@ def embedding_postprocessor(input_tensor,
     if use_position_embeddings:
         assert_op = tf.assert_less_equal(seq_length, max_position_embeddings)
         with tf.control_dependencies([assert_op]):
-            full_position_embeddings = tf.get_variable(
+            full_position_embeddings = tf.compat.v1.get_variable(
                             name=position_embedding_name,
                             shape=[max_position_embeddings, width],
                             initializer=create_initializer(initializer_range))
@@ -979,7 +979,7 @@ def assert_rank(tensor, expected_rank, name=None):
 
     actual_rank = tensor.shape.ndims
     if actual_rank not in expected_rank_dict:
-        scope_name = tf.get_variable_scope().name
+        scope_name = tf.compat.v1.get_variable_scope().name
         raise ValueError(
                         "For the tensor `%s` in scope `%s`, the actual rank "
                         "`%d` (shape = %s) is not equal to the expected rank `%s`" %
