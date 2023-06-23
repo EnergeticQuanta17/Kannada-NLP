47fe6d8d96324a8b4a9a0777cf60519a7ddeb88b
AudaciousVisionary <preetham.trh@gmail.com>
2023-06-23 10:47:04 +0530

    seeing if last token is always full stop for 10

--- POS Tagging/BERT/version7/analyze_dataset.py
+++ POS Tagging/BERT/version7/analyze_dataset.py
@@ -2,13 +2,24 @@ from tqdm import tqdm
 
 count_words = 0
 count_lines = 0
+
+words_set = set()
 with open('kn.txt', 'r') as f:
-    total_lines = sum(1 for _ in f)
-    f.seek(0)
+    total_lines = 67_265_080
 
 
     for i, line in tqdm(enumerate(f), total=total_lines):
         count_lines+=1
 
-        count_words += len(line.split(' ')) + 1
+        words = line.strip().split(' ')
+        temp1 = words[-1][:-1]
+        temp2 = words[-1][-1]
+
+        words[-1] = temp1
+        words.append(temp2)
+        print(temp2)
+
+        if(i==10):
+            raise Exception
+        words_set.update()
 print(count_lines, count_words)
