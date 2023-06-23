797b4faca170f7131ea085d01bcc79eec7bfe929
AudaciousVisionary <preetham.trh@gmail.com>
2023-06-23 11:04:32 +0530

    element and count in most common

--- POS Tagging/BERT/version7/analyze_dataset.py
+++ POS Tagging/BERT/version7/analyze_dataset.py
@@ -1,4 +1,5 @@
 from tqdm import tqdm
+from collections import Counter
 
 count_words = 0
 count_lines = 0
@@ -6,6 +7,7 @@ count_lines = 0
 empty_lines = 0
 
 words_set = set()
+list_words = []
 with open('kn.txt', 'r') as f:
     total_lines = 67_265_080
 
@@ -26,6 +28,21 @@ with open('kn.txt', 'r') as f:
                 print(words)
                 print(words[-1], ord(words[-1]))
                 input()
+
         words_set.update(words)
+        list_words.extend(words)
+
+counter = Counter(list_words)
+# most_common = sorted(counter.items(), key:lambda x:x[1], reverse=True)
+
 
 print("Count of unique words: ", len(words_set))
+print("Most frequent words: ")
+
+most_common = counter.most_common(20)
+for element, count in most_common:
+    print(element, count)
+
+
+# with open('most_frequent_words.json', 'w') as file:
+#     json.dump(most_common)
