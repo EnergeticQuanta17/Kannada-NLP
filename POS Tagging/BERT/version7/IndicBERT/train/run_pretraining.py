b4b388a8a015e697f6d537151b52bd35f22e8bf7
AudaciousVisionary <preetham.trh@gmail.com>
2023-06-23 14:18:48 +0530

    .

--- POS Tagging/BERT/version7/IndicBERT/train/run_pretraining.py
+++ POS Tagging/BERT/version7/IndicBERT/train/run_pretraining.py
@@ -405,7 +405,7 @@ def main(_):
 
     # If TPU is not available, this will fall back to normal Estimator on CPU
     # or GPU.
-    estimator = tf.contrib.tpu.TPUEstimator(
+    estimator = tf.compat.v1.estimator.tpu.TPUEstimator(
         use_tpu=FLAGS.use_tpu,
         model_fn=model_fn,
         config=run_config,
