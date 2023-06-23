719d0859076181423e234402ec6c587f426cc8b5
AudaciousVisionary <preetham.trh@gmail.com>
2023-06-23 10:22:58 +0530

    keys

--- POS Tagging/BERT/version7/download_dataset.py
+++ POS Tagging/BERT/version7/download_dataset.py
@@ -1,7 +1,11 @@
 import datasets
 
-IndicCOPA = datasets.load_dataset('ai4bharat/IndicCOPA', 'translation-kn')
+kannada_datasets = []
 
-print(IndicCOPA.keys())
-print(IndicCOPA['test'][0])
-print(type(IndicCOPA))
+# 
+IndicCOPA = datasets.load_dataset('ai4bharat/IndicCOPA', 'translation-kn')
+print("Sets in this corpus:", IndicCOPA.keys())
+print("Columns in this corpus:", IndicCOPA['test'][0].keys())
+print("Example:", IndicCOPA['test'][0])
+kannada_datasets.append(IndicCOPA)
+
