ee86a29210c2e725c185c7f1bd59dd4f62723b46
AudaciousVisionary <preetham.trh@gmail.com>
2023-06-23 10:23:28 +0530

    list

--- POS Tagging/BERT/version7/download_dataset.py
+++ POS Tagging/BERT/version7/download_dataset.py
@@ -4,8 +4,8 @@ kannada_datasets = []
 
 # 
 IndicCOPA = datasets.load_dataset('ai4bharat/IndicCOPA', 'translation-kn')
-print("Sets in this corpus:", IndicCOPA.keys())
-print("Columns in this corpus:", IndicCOPA['test'][0].keys())
+print("Sets in this corpus:", list(IndicCOPA.keys()))
+print("Columns in this corpus:", list(IndicCOPA['test'][0].keys()))
 print("Example:", IndicCOPA['test'][0])
 kannada_datasets.append(IndicCOPA)
 
