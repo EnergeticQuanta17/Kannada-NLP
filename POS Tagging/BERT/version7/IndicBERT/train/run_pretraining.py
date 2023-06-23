bf1f158bf2301a14b1585ffe6937aa8be0994475
AudaciousVisionary <preetham.trh@gmail.com>
2023-06-23 14:18:23 +0530

    estimator

--- POS Tagging/BERT/version7/IndicBERT/train/run_pretraining.py
+++ POS Tagging/BERT/version7/IndicBERT/train/run_pretraining.py
@@ -389,7 +389,7 @@ def main(_):
         master=FLAGS.master,
         model_dir=FLAGS.output_dir,
         save_checkpoints_steps=FLAGS.save_checkpoints_steps,
-        tpu_config=tf.contrib.tpu.TPUConfig(
+        tpu_config=tf.compat.v1.estimator.tpu.TPUConfig(
             iterations_per_loop=FLAGS.iterations_per_loop,
             num_shards=FLAGS.num_tpu_cores,
             per_host_input_for_training=is_per_host))
