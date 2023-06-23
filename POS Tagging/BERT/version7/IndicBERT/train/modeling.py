6e138ec39fa7e8454f36c0afa9a0481e3297f0c6
AudaciousVisionary <preetham.trh@gmail.com>
2023-06-23 14:42:45 +0530

    .

--- POS Tagging/BERT/version7/IndicBERT/train/modeling.py
+++ POS Tagging/BERT/version7/IndicBERT/train/modeling.py
@@ -193,7 +193,7 @@ class BertModel(object):
                                 max_position_embeddings=config.max_position_embeddings,
                                 dropout_prob=config.hidden_dropout_prob)
 
-            with tf.variable_scope("encoder"):
+            with tf.compat.v1.variable_scope("encoder"):
                 # This converts a 2D mask of shape [batch_size, seq_length] to a 3D
                 # mask of shape [batch_size, seq_length, seq_length] which is used
                 # for the attention scores.
@@ -221,7 +221,7 @@ class BertModel(object):
             # [batch_size, hidden_size]. This is necessary for segment-level
             # (or segment-pair-level) classification tasks where we need a fixed
             # dimensional representation of the segment.
-            with tf.variable_scope("pooler"):
+            with tf.compat.v1.variable_scope("pooler"):
                 # We "pool" the model by simply taking the hidden state corresponding
                 # to the first token. We assume that this has been pre-trained
                 first_token_tensor = tf.squeeze(self.sequence_output[:, 0:1, :], axis=1)
@@ -824,12 +824,12 @@ def transformer_model(input_tensor,
 
     all_layer_outputs = []
     for layer_idx in range(num_hidden_layers):
-        with tf.variable_scope("layer_%d" % layer_idx):
+        with tf.compat.v1.variable_scope("layer_%d" % layer_idx):
             layer_input = prev_output
 
-            with tf.variable_scope("attention"):
+            with tf.compat.v1.variable_scope("attention"):
                 attention_heads = []
-                with tf.variable_scope("self"):
+                with tf.compat.v1.variable_scope("self"):
                     attention_head = attention_layer(
                                     from_tensor=layer_input,
                                     to_tensor=layer_input,
@@ -854,7 +854,7 @@ def transformer_model(input_tensor,
 
                 # Run a linear projection of `hidden_size` then add a residual
                 # with `layer_input`.
-                with tf.variable_scope("output"):
+                with tf.compat.v1.variable_scope("output"):
                     attention_output = tf.layers.dense(
                                     attention_output,
                                     hidden_size,
@@ -863,7 +863,7 @@ def transformer_model(input_tensor,
                     attention_output = layer_norm(attention_output + layer_input)
 
             # The activation is only applied to the "intermediate" hidden layer.
-            with tf.variable_scope("intermediate"):
+            with tf.compat.v1.variable_scope("intermediate"):
                 intermediate_output = tf.layers.dense(
                                 attention_output,
                                 intermediate_size,
@@ -871,7 +871,7 @@ def transformer_model(input_tensor,
                                 kernel_initializer=create_initializer(initializer_range))
 
             # Down-project back to `hidden_size` then add the residual.
-            with tf.variable_scope("output"):
+            with tf.compat.v1.variable_scope("output"):
                 layer_output = tf.layers.dense(
                                 intermediate_output,
                                 hidden_size,
