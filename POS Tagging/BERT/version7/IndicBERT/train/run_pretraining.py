6682efcb61421143d11ea838d455838f35c6262c
AudaciousVisionary <preetham.trh@gmail.com>
2023-06-23 13:56:24 +0530

    app.run

--- POS Tagging/BERT/version7/IndicBERT/train/run_pretraining.py
+++ POS Tagging/BERT/version7/IndicBERT/train/run_pretraining.py
@@ -447,4 +447,4 @@ if __name__ == "__main__":
     flags.mark_flag_as_required("input_file")
     flags.mark_flag_as_required("bert_config_file")
     flags.mark_flag_as_required("output_dir")
-    tf.app.run()
+    tf.compat.v1.app.run()
