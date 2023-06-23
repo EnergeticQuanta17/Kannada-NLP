4863b322722b4d0d3fd238f90bdb65f737d526e5
EnergeticQuanta17 <preetham.trh@gmail.com>
2023-06-23 12:31:12 +0530

    100k

--- POS Tagging/BERT/version7/store_lines.py
+++ POS Tagging/BERT/version7/store_lines.py
@@ -1,6 +1,6 @@
 input_file = 'kn.txt'
 output_file = 'kn_100k.txt'
-line_limit = 10_000
+line_limit = 100_000
 
 with open(input_file, 'r') as input_f, open(output_file, 'w') as output_f:
     for i, line in enumerate(input_f, start=1):
