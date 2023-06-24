505c629aa27feaa2ca0308488d048e2914aaa150
EnergeticQuanta17 <preetham.trh@gmail.com>
2023-06-24 14:34:39 +0530

    trying for 1 example

--- POS Tagging/BERT/version9/second.py
+++ POS Tagging/BERT/version9/second.py
@@ -1,7 +1,26 @@
-from transformers import BertTokenizer
+from transformers import BertTokenizer, BertModel
 
 tokenizer = BertTokenizer.from_pretrained("l3cube-pune/kannada-bert")
 text = "ನನಗೆ ಒಂದು ಪ್ರಶ್ನೆ ಇದೆ"
 
 tokens = tokenizer.tokenize(text)
 print(tokens)
+
+input_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+import torch
+
+input_ids = torch.tensor([input_ids])
+attention_mask = torch.ones_like(input_ids)
+token_type_ids = torch.zeros_like(input_ids)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+input_ids = input_ids.to(device)
+attention_mask = attention_mask.to(device)
+token_type_ids = token_type_ids.to(device)
+
+model_name = "l3cube-pune/kannada-bert"
+model = BertModel.from_pretrained(model_name)
+
+model.to(device)
+outputs = model(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
