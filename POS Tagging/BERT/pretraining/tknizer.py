696c8d0ee97749de15eb0812d117e8728952b882
EnergeticQuanta17 <preetham.trh@gmail.com>
2023-06-29 15:10:47 +0530

    .encode

--- POS Tagging/BERT/pretraining/tknizer.py
+++ POS Tagging/BERT/pretraining/tknizer.py
@@ -3,10 +3,12 @@ from tokenizers import ByteLevelBPETokenizer
 input_files = ["kn_1k.txt"]
 
 tokenizer = ByteLevelBPETokenizer()
-tokenizer.train(files=input_files, vocab_size=30000, min_frequency=2, special_tokens=["[PAD]", "[UNK]", "[CLS]", "[SEP]", "[MASK]"])
+# tokenizer.train(files=input_files, vocab_size=30000, min_frequency=2, special_tokens=["[PAD]", "[UNK]", "[CLS]", "[SEP]", "[MASK]"])
 
-tokenizer.save_model("kannada_tokenizer")
+# tokenizer.save_model("kannada_tokenizer")
+
+print(dir(tokenizer))
 
 sentence = "ನಮಸ್ತೆ ಕನ್ನಡ"
-tokens = tokenizer.tokenize(sentence)
+tokens = tokenizer.encode(sentence)
 print(tokens)
