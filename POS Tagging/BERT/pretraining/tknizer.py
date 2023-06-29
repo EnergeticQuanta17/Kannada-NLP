3974232a7ebb1cabbfd108f476769cae5fba1beb
EnergeticQuanta17 <preetham.trh@gmail.com>
2023-06-29 15:02:56 +0530

    ByteLevelBPETokenizer

--- POS Tagging/BERT/pretraining/tknizer.py
+++ POS Tagging/BERT/pretraining/tknizer.py
@@ -1,8 +1,8 @@
-from transformers import BertWordPieceTokenizer
+from transformers import ByteLevelBPETokenizer
 
 input_files = ["kn_1k.txt"]
 
-tokenizer = BertWordPieceTokenizer()
+tokenizer = ByteLevelBPETokenizer()
 tokenizer.train(files=input_files, vocab_size=30000, min_frequency=2, special_tokens=["[PAD]", "[UNK]", "[CLS]", "[SEP]", "[MASK]"], only_alphabet=True)
 
 tokenizer.save_model("kannada_tokenizer")
