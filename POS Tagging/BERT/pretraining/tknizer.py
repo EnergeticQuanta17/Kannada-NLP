0f297c93437f3241e0489930a39f8aa5c6f61f91
EnergeticQuanta17 <preetham.trh@gmail.com>
2023-06-29 15:43:07 +0530

    .

--- POS Tagging/BERT/pretraining/tknizer.py
+++ POS Tagging/BERT/pretraining/tknizer.py
@@ -14,9 +14,6 @@ sentence = "ನಮಸ್ತೆ ಕನ್ನಡ"
 tokens = tokenizer.encode(sentence)
 
 for token in tokens.tokens:
-    print(bytes(token, 'utf-8'))
-    print(bytes(token, 'utf-8').decode('unicode-escape'))
-    print(bytes(token, 'utf-8').decode('unicode-escape').encode('latin-1'))
-    print(bytes(token, 'utf-8').decode('unicode-escape').encode('latin-1').decode('utf-8'))
+    print(len(token))
     
     break
