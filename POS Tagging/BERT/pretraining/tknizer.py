4524ed5b0daa427e6d9824e812ba4ca6e8835901
EnergeticQuanta17 <preetham.trh@gmail.com>
2023-06-29 15:36:24 +0530

    .

--- POS Tagging/BERT/pretraining/tknizer.py
+++ POS Tagging/BERT/pretraining/tknizer.py
@@ -10,8 +10,8 @@ tokenizer.save_model("kannada_tokenizer")
 # print(dir(tokenizer))
 
 sentence = "ನಮಸ್ತೆ ಕನ್ನಡ"
-sentence = "ಡ"
-print(chr(ord(sentence)))
+# sentence = "ಡ"
+# print(chr(ord(sentence)))
 print(sentence, sentence.encode('utf-8').decode('utf-8'))
 tokens = tokenizer.encode(sentence)
 print(type(tokens.tokens))
