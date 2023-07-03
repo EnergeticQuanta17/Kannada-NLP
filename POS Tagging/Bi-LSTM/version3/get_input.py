3bbc12d959f00478f4acb10cc38a5fd632c84d35
AudaciousVisionary <preetham.trh@gmail.com>
2023-07-03 11:38:07 +0530

    GI

--- POS Tagging/Bi-LSTM/version3/get_input.py
+++ POS Tagging/Bi-LSTM/version3/get_input.py
@@ -5,14 +5,16 @@ with open('../../../Parsing/AnnotatedDatasetParsing/full_dataset_131.pickle', 'r
 
 print("Number of sentences retrieved: ", len(retrieved_sentences))
 
+SENTENCE_INDEX = 1
+
 words = []
 tags = []
 
-for sentence in retrieved_sentences:
-    for chunk in sentence.list_of_chunks:
-        for word in chunk.list_of_words:
-            words.append(word.kannada_word)
-            tags.append(word.pos)
+sentence = retrieved_sentences[SENTENCE_INDEX]
+for chunk in sentence.list_of_chunks:
+    for word in chunk.list_of_words:
+        words.append(word.kannada_word)
+        tags.append(word.pos)
     break
 
 print(words, tags)
